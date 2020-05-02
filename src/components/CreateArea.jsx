50db2cf4cb43ecc8786d0b8d7a9d7fbd64fff879
Evgeniy Grinko <john_forever_young@mail.ru>
2020-05-02 15:38:51 +0300

    Add pre-built React components and visual representation
    Added packages "@material-ui/core" and "@material-ui/icons".
    "@material-ui/core" - allows to use pre-built React components from Google Material Design.
    "@material-ui/icons" - the Google Material icons packaged as a set of React components.
    
    Main changes:
    -in style.css: added background image (from www.transparenttextures.com);
    -in Header.jsx and Note.jsx: added icons ("HighlightIcon" and "DeleteIcon" respectively);
    -in CreateArea.jsx: added icon ("AddIcon"), pre-built React components ("Zoom" - for button appearance, "Fab" - to react for mouse hover),
    added state "isExpanded" to expand initial textarea when the user will click on it (to increase interactivity).

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  
   const [fullNote, setFullNote] = useState({
     title: "",
     content: ""
   });
+
+  const [isExpanded, setExpanded] = useState(false);
 
   function handleNote(event) {
     const { name, value } = event.target;
@@ -25,23 +31,33 @@ function CreateArea(props) {
     });
   }
 
+  function handleInitialClick(){
+    setExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          placeholder="Title"
-          value={fullNote.title}
-          onChange={handleNote}
-        />
+      <form className="create-note">
+      {/* before the user will click on "textarea", this "title-input" will be hidden, after that - visible */}
+        {isExpanded && <input
+                          name="title"
+                          placeholder="Title"
+                          value={fullNote.title}
+                          onChange={handleNote}
+                        />}
         <textarea
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1} //before the user will click on "textarea" it will be small (1 row), after click - expanded (3 rows)
           value={fullNote.content}
           onChange={handleNote}
+          onClick = {handleInitialClick}
         />
-        <button onClick={handleClick}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={handleClick}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Note(props) {
+
+  function handleClick(){
+    props.deleteOneNote(props.id);
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={() => props.deleteOneNote(props.id)}>DELETE</button>
+      <button onClick={handleClick}><DeleteIcon/></button>
     </div>
   );
 }
